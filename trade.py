19453d4e8214f966e48c70d70d65c3050ba8b3fa
Alex7861 <alex.ruperto@javierlabs.com>
2023-09-05 04:54:53 -0400

    added more constructor level variables for when the strategy is instantiated. particularly a condition for golden and death cross, lists for buy and sell dates, pending order, and crossover history. consistent_crossover whether a crossover trend remains true or not, then if it stays true, and the pending order is a buy, execute the buy order and append it to the datetime of the order.

--- trade.py
+++ trade.py
@@ -19,35 +19,64 @@ class SMACrossover(bt.Strategy):  # class definition with bt.Strategy as the par
         # parameters. The crossover indicator is checking for crossovers between two data lines.
         # + 1 for upward, -1 for downward crossover
         self.position_sizes = [self.position.size] * self.p.long_period
-        self.cash_value = [
-                              self.broker.get_cash()] * self.p.long_period  # multiply this cash value list by the long period
-        self.account_values = [
-                                  self.broker.get_value()] * self.p.long_period  # multiply this asset value list by the long period
-        self.sma_short = bt.ind.SMA(
-            period=self.p.short_period)  # initialize sma_short as a bt SMA indicator using the short period
-        self.sma_long = bt.ind.SMA(
-            period=self.p.long_period)  # initialize sma_long as a bt SMA indicator using the long period
-        self.crossover = bt.ind.CrossOver(self.sma_short,
-                                          self.sma_long)  # use crossover indicator with sma_short and sma_long
+        self.cash_value = [self.broker.get_cash()] * self.p.long_period  # multiply this cash value list by the long period
+        self.account_values = [self.broker.get_value()] * self.p.long_period  # multiply this asset value list by the long period
+        self.sma_short = bt.ind.SMA(self.data.close, period=self.p.short_period)  # initialize sma_short as a bt SMA indicator using the short period
+        self.sma_long = bt.ind.SMA(self.data.close, period=self.p.long_period)  # initialize sma_long as a bt SMA indicator using the long period
+        self.crossover = bt.ind.CrossOver(self.sma_short, self.sma_long)  # use crossover indicator with sma_short and sma_long
+        self.crossover_history = [] # record the history of the crossover into a list
+        self.buy_dates = [] # stores the buy dates
+        self.sell_dates = [] # stores the sell dates
+        self.pending_order = None # tracks the buy/sell decision
+        self.in_golden_cross = False
+        self.in_death_cross = False
 
     # call next method for each bar/candle in backtest.
     def next(self):
-        self.cash_value.append(self.broker.get_cash())  # append the current cash value to the list.
-        self.account_values.append(self.broker.get_value())
-        size_to_buy = (self.broker.get_cash() * 0.1) // self.data.close[0]  # take 10% of your cash and then perform floor division by the closing price of the stock.
+        # Add the crossover to the history first
+        self.crossover_history.append(self.crossover[0]) # append self.crossover to the crossover_history list
+
+        # update the states
+        # Golden Cross. Short SMA Crosses over Long SMA
+        if self.sma_short[0] > self.sma_long[0] and not self.in_golden_cross: 
+            self.in_golden_cross = True
+            self.in_death_cross = False
+            self.pending_order = 'buy'
+
+        # Death Cross. Short SMA Crosses under Long SMA
+        elif self.sma_short[0] < self.sma_long[0] and not self.in_death_cross:
+            self.in_golden_cross = False
+            self.in_death_cross = True
+            self.pending_order = 'sell'
+
+        # Add a variable window to confirm trend
+        confirmation_days = 5 # !!!keep in mind, execution will take an extra day!!!
+        consistent_crossover = all(x == self.crossover[0] for x in self.crossover_history[-confirmation_days:])
+        # Check the last confirmation_days - 1 days since we've already added today's value\
+        # - in confirmation_days is used to count from the end of the list backwards. confirm each item in
+        # this list is ('x') to the current crossover value. it will return true or false. the all return
+        # true if all the items in the iterable return true. otherwise, false.
+        # colon indicates a slice from starting point to the end.
+
+         # take 10% of your cash and then perform floor division by the closing price of the stock.
+        size_to_buy = (self.broker.get_cash() * 0.1) // self.data.close[0]  
         if size_to_buy < 1:  # if the size_to_buy value is less than one
             size_to_buy = 1  # buy one
 
-        # Add a 3-day window to confirm trend
-        confirmation_days = 3
+        if self.pending_order == 'buy' and consistent_crossover: # if the pending order is a buy, 
+            self.buy(size=size_to_buy) # buy
+            self.buy_dates.append(bt.num2date(self.data.datetime[0])) # add the date of when it bought
+            self.pending_order = None # reset pending order
 
-        if self.crossover > 0 and sum([self.crossover[i] for i in range(-confirmation_days, 0)]) == confirmation_days:  # Golden Cross. Short SMA Crosses over Long SMA
-            # this is a check to see short SMA crossed over.
-            self.buy(size=size_to_buy)
-        elif self.crossover < 0 < self.position.size:  # Death Cross. Short SMA Crosses under Long SMA
-            self.sell()
+        elif self.pending_order == 'sell' and consistent_crossover:
+            self.sell() # sell
+            self.sell_dates.append(bt.num2date(self.data.datetime[0])) # add the date of when it sold
+            self.pending_order = None # reset pending order
 
-        self.position_sizes.append(self.position.size)
+        self.cash_value.append(self.broker.get_cash())  # append the current cash value to the list.
+        self.account_values.append(self.broker.get_value()) # append the current account value list
+        self.position_sizes.append(self.position.size) # append the current position size to list
+        
 
 
 def main():
@@ -56,14 +85,14 @@ def main():
     # broker simulation, etc/
 
     # Fetch historical data
-    data = yf.download('MRNA', start='2019-01-01', end='2023-09-01')  # collect data from MRNA at given time range
+    data = yf.download('TSLA', start='2019-01-01', end='2023-09-01')  # collect data from MRNA at given time range
     datafeed = bt.feeds.PandasData(dataname=data)  # convert data into format that cerebro can understand
     cerebro.adddata(datafeed)  # add datafeed to cerebro
 
     cerebro.addstrategy(SMACrossover)  # use SMACrossover strategy for the backtest.
 
     # Set our desired cash start
-    cerebro.broker.set_cash(1000.0)
+    cerebro.broker.set_cash(10000.0)
 
     # Set the commission
     cerebro.broker.setcommission(commission=0.001)  # 0.1% commission on trades
@@ -85,16 +114,15 @@ def main():
     cash_value = strategy.cash_value
     account_values = strategy.account_values
     position_sizes = strategy.position_sizes
+    
 
     # For buy/sell markers
-    buys_x = [dates[i] for i, val in enumerate(strategy.crossover.array) if
-              val > 0]  # only considers values that are positive. for each one, store corresponding date from the dates list using i. (buy signal)
-    buys_y = [closes[i] for i, val in enumerate(strategy.crossover.array) if
-              val > 0]  # only considers values that are positive. for each one, store corresponding closing price from the dates list using i. (buy signal)
-    sells_x = [dates[i] for i, val in enumerate(strategy.crossover.array) if
-               val < 0]  # only considers values that are negative. for each one, store corresponding date from the dates list using i. (sell signal)
-    sells_y = [closes[i] for i, val in enumerate(strategy.crossover.array) if
-               val < 0]  # only considers values that are negative. for each one, store corresponding closing price from the dates list using i. (sell signal)
+    buys_x = strategy.buy_dates # set buys_x to the buy dates. 
+    # for each date in buy_dates, find the associated closing price from the 'closes' list and add it to buy_y
+    buys_y = [closes[dates.index(date)] for date in buys_x if date in dates]
+    sells_x = strategy.sell_dates
+    # for each date in sell_dates, find the associated closing price from the 'closes' list and add it to sell_y
+    sells_y = [closes[dates.index(date)] for date in sells_x if date in dates]
 
     # create plotly plot
     fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.1,
@@ -110,10 +138,10 @@ def main():
     fig.add_trace(
         go.Scatter(x=dates, y=np.array(sma_long).tolist(), mode='lines', name='200-day SMA', legend='legend1'), row=1,
         col=1)  # Plot 100-day SMA on row 1 col 1
-    fig.add_trace(go.Scatter(x=buys_x, y=buys_y, mode='markers', marker=dict(color='green', size=15), name='Buy Signal',
+    fig.add_trace(go.Scatter(x=buys_x, y=buys_y, mode='markers', marker=dict(color='green', size=15), name='Buy Order',
                              legend='legend1'), row=1, col=1)  # Plot the buys on row 1 col 1
     fig.add_trace(
-        go.Scatter(x=sells_x, y=sells_y, mode='markers', marker=dict(color='red', size=15), name='Sell Signal',
+        go.Scatter(x=sells_x, y=sells_y, mode='markers', marker=dict(color='red', size=15), name='Sell Order',
                    legend='legend1'), row=1, col=1)  # Plot the sells on row 1 col 1
 
     # Second Plot (Portfolio Value)
