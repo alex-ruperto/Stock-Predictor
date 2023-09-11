2b2007f88f90dbf70f1e8f183121cf73133afc7f
Alex7861 <alex.ruperto@javierlabs.com>
2023-09-11 02:28:20 -0400

    Separated SMACrossover strategy class to clean up trade.py. Also encourages separating each strategy class into its own file. Also added the 'ticker' so that the user can see which stock is being tested inplotly, as well as use it to easily control which stock to run the backtest on.

--- trade.py
+++ trade.py
@@ -4,80 +4,7 @@ import backtrader as bt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
-
-
-class SMACrossover(bt.Strategy):  # class definition with bt.Strategy as the parent
-    # these two periods will refer to how many days the moving average will be calculated.
-    # one moving average for a short period and one for a long period.
-    params = dict(
-        short_period=50,
-        long_period=200
-    )
-
-    def __init__(self):  # constructor for when the SMACrossover class is called.
-        # create short simple moving average using short_period and long simple moving average using long_period
-        # parameters. The crossover indicator is checking for crossovers between two data lines.
-        # + 1 for upward, -1 for downward crossover
-        self.position_sizes = [self.position.size] * self.p.long_period
-        self.cash_value = [self.broker.get_cash()] * self.p.long_period  # multiply this cash value list by the long period
-        self.account_values = [self.broker.get_value()] * self.p.long_period  # multiply this asset value list by the long period
-        self.sma_short = bt.ind.SMA(self.data.close, period=self.p.short_period)  # initialize sma_short as a bt SMA indicator using the short period
-        self.sma_long = bt.ind.SMA(self.data.close, period=self.p.long_period)  # initialize sma_long as a bt SMA indicator using the long period
-        self.crossover = bt.ind.CrossOver(self.sma_short, self.sma_long)  # use crossover indicator with sma_short and sma_long
-        self.crossover_history = [] # record the history of the crossover into a list
-        self.buy_dates = [] # stores the buy dates
-        self.sell_dates = [] # stores the sell dates
-        self.pending_order = None # tracks the buy/sell decision
-        self.in_golden_cross = False # boolean to check whether it is in a golden cross or not
-        self.in_death_cross = False # boolean to check whether it is in a death cross or not
-
-    # call next method for each bar/candle in backtest.
-    def next(self):
-        # Add the crossover to the history first
-        self.crossover_history.append(self.crossover[0]) # append self.crossover to the crossover_history list
-
-        # update the states
-        # Golden Cross. Short SMA Crosses over Long SMA
-        if self.sma_short[0] > self.sma_long[0] and not self.in_golden_cross: 
-            self.in_golden_cross = True
-            self.in_death_cross = False
-            self.pending_order = 'buy'
-
-        # Death Cross. Short SMA Crosses under Long SMA
-        elif self.sma_short[0] < self.sma_long[0] and not self.in_death_cross:
-            self.in_golden_cross = False
-            self.in_death_cross = True
-            self.pending_order = 'sell'
-
-        # Add a variable window to confirm trend
-        confirmation_days = 3 # !!!keep in mind, execution will take an extra day!!!
-        consistent_crossover = all(x == self.crossover[0] for x in self.crossover_history[-confirmation_days:])
-        # Check the last confirmation_days - 1 days since we've already added today's value\
-        # - in confirmation_days is used to count from the end of the list backwards. confirm each item in
-        # this list is ('x') to the current crossover value. it will return true or false. the all return
-        # true if all the items in the iterable return true. otherwise, false.
-        # colon indicates a slice from starting point to the end.
-
-         # take 10% of your cash and then perform floor division by the closing price of the stock.
-        size_to_buy = (self.broker.get_cash() * 0.1) // self.data.close[0]  
-        if size_to_buy < 1:  # if the size_to_buy value is less than one
-            size_to_buy = 1  # buy one
-
-        if self.pending_order == 'buy' and consistent_crossover: # if the pending order is a buy and consistent_crossover
-            self.buy(size=size_to_buy) # buy
-            self.buy_dates.append(bt.num2date(self.data.datetime[0])) # add the date of when it bought
-            self.pending_order = None # reset pending order
-
-        elif self.pending_order == 'sell' and consistent_crossover and self.position.size > 0: 
-            # if the pending_order is a sell, consistent and there are shares in the account, 
-            self.sell() # sell
-            self.sell_dates.append(bt.num2date(self.data.datetime[0])) # add the date of when it sold
-            self.pending_order = None # reset pending order
-
-        self.cash_value.append(self.broker.get_cash())  # append the current cash value to the list.
-        self.account_values.append(self.broker.get_value()) # append the current account value list
-        self.position_sizes.append(self.position.size) # append the current position size to list
-        
+from SMACrossover import SMACrossoverStrategy
 
 
 def main():
@@ -86,11 +13,12 @@ def main():
     # broker simulation, etc/
 
     # Fetch historical data
-    data = yf.download('MRNA', start='2019-01-01', end='2023-09-01')  # collect data from MRNA at given time range
+    ticker = 'MRNA'
+    data = yf.download(ticker, start='2019-01-01', end='2023-09-01')  # collect data from MRNA at given time range
     datafeed = bt.feeds.PandasData(dataname=data)  # convert data into format that cerebro can understand
     cerebro.adddata(datafeed)  # add datafeed to cerebro
 
-    cerebro.addstrategy(SMACrossover)  # use SMACrossover strategy for the backtest.
+    cerebro.addstrategy(SMACrossoverStrategy)  # use SMACrossover strategy for the backtest.
 
     # Set our desired cash start
     cerebro.broker.set_cash(10000.0)
@@ -127,7 +55,7 @@ def main():
 
     # create plotly plot
     fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.1,
-                        subplot_titles=('Trading Data', 'Portfolio Value', 'Position Over Time'))
+                        subplot_titles=(f'Trading Data for {ticker}', 'Portfolio Value', 'Position Over Time'))
 
     # First plot (Trading Data)
     # Side note: The legend tag is simply to help with te alignment in fig.update_layout.
