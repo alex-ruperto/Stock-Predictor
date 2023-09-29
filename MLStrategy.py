b658881eee904050c774fe8918c854f2f0ab65ed
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-09-28 22:20:22 -0400

    Added stochastic and bollinger indicators

--- MLStrategy.py
+++ MLStrategy.py
@@ -1,6 +1,7 @@
 import backtrader as bt
 from BaseStrategy import BaseStrategy
 import pandas as pd
+from ml_models import bollinger_bands, stochastic_oscillator
 
 class MLStrategy (BaseStrategy):
     params = [
@@ -18,11 +19,24 @@ class MLStrategy (BaseStrategy):
         # MACD
         self.macd = bt.indicators.MACD(self.data.close, period_me1=12, period_me2=26, period_signal=9)
         
+        # Bollinger Bands
+        self.bollinger = bt.indicators.BollingerBands(self.data.close, period=20, devfactor=2)
+        self.upper_bollinger = self.bollinger.lines.top
+        self.lower_bollinger = self.bollinger.lines.bot
+
+        # Stochastic Oscillator
+        self.stochastic = bt.indicators.Stochastic(self.data)
+        self.k_line = self.stochastic.lines.percK
+        self.d_line = self.stochastic.lines.percD
+        
     def next(self):
-        current_data = [self.sma_short[0], self.sma_long[0], self.rsi[0], self.macd.macd[0], self.macd.signal[0]]
+        current_data = [
+            self.sma_short[0], self.sma_long[0], self.rsi[0], self.macd.macd[0], self.macd.signal[0],
+            self.upper_bollinger[0], self.lower_bollinger[0], self.k_line[0], self.d_line[0]
+        ]
         prediction = None
         if self.params.model is not None:
-            df = pd.DataFrame([current_data], columns=['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line'])
+            df = pd.DataFrame([current_data], columns=['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line'])
             prediction = self.params.model.predict(df)[0]
         
         if prediction == 1:  # If the model predicts the stock will go up.
--- ml_models.py
+++ ml_models.py
@@ -9,10 +9,13 @@ def train_model(df): # pandas dataframe expected
     # handle missing values using mean imputation
     imputer = SimpleImputer(strategy='mean')
     df_imputed = df.copy()
-    df_imputed[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line']] = imputer.fit_transform(df[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line']])
+    df_imputed[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 
+                'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 
+                'D_Line']] = imputer.fit_transform(df[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 
+                'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line']])
     
     # split data into training and test sets:
-    X = df_imputed[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line']]
+    X = df_imputed[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line']]
     y = df_imputed['Target']
 
 
@@ -26,7 +29,7 @@ def train_model(df): # pandas dataframe expected
         'max_depth':[None, 10, 20], # Maximum depth of each tree
         'min_samples_split':[2, 5], # minimum number of samples to split an node. if samples < this, become leaf node
         'min_samples_leaf':[1, 2], # minimum number of samples required to be at a leaf node
-        'max_features':['sqrt'] # determines maximum number of features considered
+        'max_features':[None] # determines maximum number of features considered
     }
     # number of candidates = multiplication of the size of each hyperparameter. 
     # number of fits (model training runs) = number of candidates * number of folds (cv)
@@ -56,6 +59,7 @@ def train_model(df): # pandas dataframe expected
     
     return best_clf
 
+# features
 def preprocess_data(df):
     df['SMA1'] = df['Close'].rolling(window=50).mean()
     df['SMA2'] = df['Close'].rolling(window=200).mean()
@@ -63,6 +67,9 @@ def preprocess_data(df):
     df['Target'] = (df['Close'].shift(-1) > df['Close']).astype(int) 
     df['RSI'] = rsi_formula(df['Close'], window=14)
     df['MACD_Line'], df['Signal_Line'] = macd_formula(df['Close'], short_window=12, long_window=26, signal_window=9)
+    df['Upper_Bollinger'], df['Lower_Bollinger'] = bollinger_bands(df['Close'])
+    df['K_Line'], df['D_Line'] = stochastic_oscillator(df['Close'])
+
     return df
 
 def rsi_formula(data, window):
@@ -85,3 +92,19 @@ def macd_formula(data, short_window, long_window, signal_window):
     signal_line = macd_line.ewm(span=signal_window, adjust=False).mean()
     
     return macd_line, signal_line
+
+def bollinger_bands(data, window=20, num_std_dev=2):
+    """Computer Bollinger Bands"""
+    rolling_mean = data.rolling(window=window).mean()
+    rolling_std = data.rolling(window=window).std()
+    upper_band = rolling_mean + (rolling_std * num_std_dev)
+    lower_band = rolling_mean - (rolling_std * num_std_dev)
+    return upper_band, lower_band
+
+def stochastic_oscillator(data, window=14):
+    """Computer Stochastic Oscillator"""
+    low_min = data.rolling(window=window).min()
+    high_max = data.rolling(window=window).max()
+    k_line = 100 * ((data - low_min) / (high_max - low_min))
+    d_line = k_line.rolling(window=3).mean()
+    return k_line, d_line
