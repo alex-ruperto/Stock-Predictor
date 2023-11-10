05a5a3002939319791769e15d01a6ca2ada232de
Alex <alex.ruperto@javierlabs.com>
2023-11-10 16:13:28 -0500

    Bot is still primarily guessing either fully bullish or fully bearish predictions despite all the new features.

--- lstm_model.py
+++ lstm_model.py
@@ -3,15 +3,19 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim import SGD
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import f1_score
 from torch.utils.data import DataLoader, TensorDataset
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import StandardScaler
 import pandas_ta
+
+time_interval = 6.5 # Adjust this to whatever the time interval from raw_data in data_processing is. There are 6.5 hourly interval candles in a single trading day.
 
 # features
 def preprocess_data(df):
-    time_interval = 6.5 # adjust this to whatever the time interval from raw_data in data_processing is. there are 26 15 minute interval candles in a single trading day.
     # compare next day closing price to current day. convert boolean values to integer values
     df['Target'] = (df['Close'].shift(-1) > df['Close']).astype(int) # df['Close] is closing price for each candle. target is to get next close higher than current.
     # Simple Moving Averages 1 and 2
@@ -46,7 +50,29 @@ def preprocess_data(df):
     d_line_col = stoch.columns[1]  # %D line is typically the second column
     df['K_Line'] = stoch[k_line_col]
     df['D_Line'] = stoch[d_line_col]
-    
+
+    # Exponential Moving Averages
+    df['EMA1'] = pandas_ta.ema(df['Close'], length=12*time_interval)
+    df['EMA2'] = pandas_ta.ema(df['Close'], length=26*time_interval)
+
+    # On-Balance Volume
+    df['OBV'] = pandas_ta.obv(df['Close'], df['Volume'])
+    scaler = StandardScaler()
+    df['OBV_Scaled'] = scaler.fit_transform(df[['OBV']])
+    df = df.drop('OBV', axis=1)
+
+    # Volume-weighted Average Price (VWAP)
+    df['VWAP'] = pandas_ta.vwap(high=df['High'], low=df['Low'], close=df['Close'], volume=df['Volume'], length=14*time_interval)
+
+    # Historical Volatility
+    df['Volatility'] = pandas_ta.stdev(df['Close'], length=14*time_interval)
+
+    # Price Rate of Change
+    df['ROC'] = pandas_ta.roc(df['Close'], length=10*time_interval)
+
+    # Average True Range
+    df['ATR'] = pandas_ta.atr(df['High'], df['Low'], df['Close'], length=14*time_interval)
+
     return df
 
 # end of features
@@ -95,32 +121,63 @@ class LSTMModel(nn.Module): # nn module is the base class for all neural network
 '''
 ------------------------------------------------Train Validate Test------------------------------------------------
 '''
-def train_model(df, epochs=50, batch_size=32, learning_rate=0.001):
-    # Handle missing values using mean imputation
-    imputer = SimpleImputer(strategy='mean')
-    df_imputed = df.copy()
-    df_imputed[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 
-                'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 
-                'D_Line']] = imputer.fit_transform(df[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 
-                'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line']])
-    X = df_imputed[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line']]
-    y = df_imputed['Target']
+def train_model(df, epochs=30, batch_size=64, learning_rate=0.1):
+    # Define warm-up periods for each feature
+    feature_warm_up_periods = {
+        'Target': 0,
+        'SMA1': int(50 * time_interval),
+        'SMA2': int(200 * time_interval),
+        'RSI': int(14 * time_interval),
+        'MACD_Line': int(50 * time_interval),
+        'Signal_Line': int(9 * time_interval),
+        'Upper_Bollinger': int(20 * time_interval),
+        'Lower_Bollinger': int(20 * time_interval),
+        'K_Line': int(14 * time_interval),
+        'D_Line': int(3 * time_interval),
+        'EMA1': int(12 * time_interval),
+        'EMA2': int(26 * time_interval),
+        'OBV_Scaled': 0,
+        'VWAP': int(14 * time_interval),
+        'Volatility': int(14 * time_interval),
+        'ROC': int(10 * time_interval),
+        'ATR': int(14 * time_interval)
+    }
 
-    X_train_tensor, y_train_tensor, X_val_tensor, y_val_tensor, X_test_tensor, y_test_tensor = prepare_data_for_lstm(X, y)
+    def dynamically_introduce_features(df, feature_periods):
+        max_warm_up = max(feature_periods.values())
+        dynamic_df = pd.DataFrame(index=df.index[max_warm_up:])
+
+        for feature, warm_up in feature_periods.items():
+            dynamic_df[feature] = df[feature].iloc[warm_up:]
+        
+        return dynamic_df.dropna()
+
+    def find_best_threshold(y_true, y_scores):
+        fpr, tpr, thresholds = roc_curve(y_true, y_scores)
+        optimal_idx = np.argmax(tpr - fpr)
+        optimal_threshold = thresholds[optimal_idx]
+
+        return optimal_threshold  # Return only the threshold value
+    
+    df_dynamic = dynamically_introduce_features(df, feature_warm_up_periods)
+    
+    X = df_dynamic[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line', 'EMA1', 'EMA2', 'OBV_Scaled', 'VWAP', 'Volatility', 'ROC', 'D_Line']]
+    y = df_dynamic['Target']
+
     # Prepare the data
+    X_train_tensor, y_train_tensor, X_val_tensor, y_val_tensor, X_test_tensor, y_test_tensor = prepare_data_for_lstm(X, y)
     train_data = TensorDataset(X_train_tensor, y_train_tensor)
     train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
 
 
-######################### Create, Train, Test, and Validate LSTM Model #########################
-    model = LSTMModel(X_train_tensor.shape[-1], 50)
+######################### Create, Train, Validate and Test LSTM Model #########################
+    model = LSTMModel(X_train_tensor.shape[-1], 200)
     criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = SGD(model.parameters(), lr=learning_rate, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-    best_threshold = 0.5
 
     # Training loop
-    for epoch in range(epochs): # update model's weights 50 times. each epoch, it will start with the weights of the previous epoch. the hope is to reduce the amount of loss each time.
+    for epoch in range(epochs): # update model's weights (epochs) times. each epoch, it will start with the weights of the previous epoch. the hope is to reduce the amount of loss each time.
         model.train() # set the model to train mode.
         for X_batch, y_batch in train_loader:
             optimizer.zero_grad()
@@ -132,36 +189,22 @@ def train_model(df, epochs=50, batch_size=32, learning_rate=0.001):
 
         # Validate for hyperparameter tuning
         model.eval()
-        with torch.no_grad(): # no gradients needed for validation phase
+        with torch.no_grad():
             val_predictions_logits = model(X_val_tensor).squeeze()
-            val_predictions = torch.sigmoid(val_predictions_logits)  # Apply sigmoid here
+            val_predictions = torch.sigmoid(val_predictions_logits)
+            y_val_binary_np = y_val_tensor.int().numpy()
 
-            # Convert the y_val_tensor into a binary value
-            y_val_binary = y_val_tensor.int()
+            # Find the best threshold using ROC curve analysis
+            best_threshold = find_best_threshold(y_val_binary_np, val_predictions.numpy())
 
-            best_f1_score = 0
-            # Find the best threshold for the current epoch
-            for threshold in np.arange(0.1, 0.9, 0.01):
-                binary_predictions = (val_predictions > threshold).int()
-                binary_predictions_np = binary_predictions.numpy()
-                y_val_binary_np = y_val_binary.numpy()
-
-                # Calculate F1 score
-                current_f1_score = f1_score(y_val_binary_np, binary_predictions_np)
-                
-                # Track the best F1 score and save the threshold
-                if current_f1_score > best_f1_score:
-                    best_f1_score = current_f1_score
-                    best_threshold = threshold
-            
-        # After finding the best threshold, calculate the accuracy with this threshold
-        y_val_binary_squeezed = y_val_binary.squeeze()
-        best_binary_predictions = (val_predictions > best_threshold).int()
-        correct_val_predictions = (best_binary_predictions == y_val_binary_squeezed).float().sum()
-        val_accuracy = correct_val_predictions / y_val_binary.size(0)
+            # Calculate accuracy using the best threshold
+            y_val_binary_squeezed = y_val_tensor.squeeze()
+            best_binary_predictions = (val_predictions > best_threshold).int()
+            correct_val_predictions = (best_binary_predictions == y_val_binary_squeezed).float().sum()
+            val_accuracy = correct_val_predictions / y_val_binary_squeezed.size(0)
         
         # Print epoch results
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item()}, Val F1 Score: {best_f1_score:.4f}, Val Accuracy: {val_accuracy:.4f}, Best Threshold: {best_threshold:.2f}, Correct val predictions: {correct_val_predictions}, y-val binary size: {y_val_binary.size(0)}")
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item()}, Val Accuracy: {val_accuracy:.4f}, Best Threshold: {best_threshold:.2f}, Correct val predictions: {correct_val_predictions}, y-val binary size: {y_val_binary_squeezed.size(0)}")
 
     # Calculate test accuracy
     model.eval() 
--- test_lstm.py
+++ test_lstm.py
@@ -10,53 +10,50 @@ class TestFeatures(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.time_interval = 6.5 # this is the actual time interval used in the data.
-        ticker = "GOOG"
+        ticker = "AMC"
         end_date = datetime.now().strftime('%Y-%m-%d')
         start_date = (datetime.now() - timedelta(days=729)).strftime('%Y-%m-%d')
         cls.data = yf.download(ticker, start = start_date, end = end_date, interval='1h', auto_adjust=True)
     
-    def test_preprocess_data(self):
-        warm_up_period = math.ceil(200*self.time_interval)
-        # Apply the preprocessing
-        processed_data = preprocess_data(self.data)
+    # def test_preprocess_data(self):
+    #     warm_up_period = math.ceil(200*self.time_interval)
+    #     # Apply the preprocessing
+    #     processed_data = preprocess_data(self.data)
 
-        # Check that the returned DataFrame is not empty
-        self.assertTrue(processed_data.iloc[:warm_up_period].isnull().values.any(), "Expected NaN values in warm-up period")
-        self.assertFalse(processed_data.iloc[warm_up_period:].isnull().values.any(), "Unexpected NaN values found after the warm-up period")
+    #     # Check that the returned DataFrame is not empty
+    #     self.assertTrue(processed_data.iloc[:warm_up_period].isnull().values.any(), "Expected NaN values in warm-up period")
+    #     self.assertFalse(processed_data.iloc[warm_up_period:].isnull().values.any(), "Unexpected NaN values found after the warm-up period")
 
 
     
-    def test_data_quality(self):
-        # Convert the index to a column to check for missing date-times
-        self.data.reset_index(inplace=True)
+    # def test_data_quality(self):
+    #     # Convert the index to a column to check for missing date-times
+    #     self.data.reset_index(inplace=True)
 
-        # Check for missing values
-        missing_values = self.data.isnull().sum()
-        self.assertFalse(missing_values.any(), "There are missing values in the dataset")
+    #     # Check for missing values
+    #     missing_values = self.data.isnull().sum()
+    #     self.assertFalse(missing_values.any(), "There are missing values in the dataset")
 
-        # Check for duplicates
-        duplicate_rows = self.data[self.data.duplicated()]
-        self.assertTrue(duplicate_rows.empty, "There are duplicate rows in the dataset")
-
-
-        # Statistical summary to check for outliers and inconsistencies
-        statistical_summary = self.data.describe()
-        print(f"Statistical summary:\n{statistical_summary}")
-
-        # Check for the correct data type of each column except for the datetime one.
-        numeric_columns = self.data.select_dtypes(include=['float64', 'int64'])
-        self.assertTrue(numeric_columns.shape[1] == self.data.shape[1] - 1, "Not all data types are numeric")
+    #     # Check for duplicates
+    #     duplicate_rows = self.data[self.data.duplicated()]
+    #     self.assertTrue(duplicate_rows.empty, "There are duplicate rows in the dataset")
 
 
-'''
+    #     # Statistical summary to check for outliers and inconsistencies
+    #     statistical_summary = self.data.describe()
+    #     print(f"Statistical summary:\n{statistical_summary}")
+
+    #     # Check for the correct data type of each column except for the datetime one.
+    #     numeric_columns = self.data.select_dtypes(include=['float64', 'int64'])
+    #     self.assertTrue(numeric_columns.shape[1] == self.data.shape[1] - 1, "Not all data types are numeric")
+
+
     def test_test_accuracy(self):
             df = preprocess_data(self.data)
-
-            # Handle missing values using mean imputation and prepare data
-            X, y = df[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line']], df['Target']
+            X, y = df[['SMA1', 'SMA2', 'RSI', 'MACD_Line', 'Signal_Line', 'Upper_Bollinger', 'Lower_Bollinger', 'K_Line', 'D_Line', 'EMA1', 'EMA2', 'OBV_Scaled', 'VWAP', 'Volatility', 'ROC', 'ATR']], df['Target']
             X_train_tensor, y_train_tensor, X_val_tensor, y_val_tensor, X_test_tensor, y_test_tensor = prepare_data_for_lstm(X, y)
 
-            model, best_threshold = train_model(df, epochs=50)
+            model, best_threshold = train_model(df, epochs=30)
 
             model.eval()
             with torch.no_grad():
@@ -66,9 +63,17 @@ class TestFeatures(unittest.TestCase):
                 correct_test_predictions = (best_test_predictions == y_test_tensor.int()).float().sum()
                 test_accuracy = correct_test_predictions / len(y_test_tensor)
 
+            # Convert tensors to numpy arrays for easy handling
+            test_predictions_np = best_test_predictions.numpy()
+            y_test_np = y_test_tensor.squeeze().numpy()
+
+            # Print the first 50 predictions and actual values
+            print("First 100 Predictions:", test_predictions_np[:100])
+            print("First 100 Actual Values:", y_test_np[:100])
+
             # Assert that the test accuracy is above a certain threshold
             self.assertGreater(test_accuracy.item(), 0.5, "Test accuracy should be greater than 50%")
-'''
+
     
 
 if __name__ == '__main__':
